63d58123031cafcc553ca56b73fa0dd02d1566f6
DariaPressley <daria.mik@gmail.com>
2023-10-16 21:10:25 -0700

    adding markdown sheet and code

---
+++ index.js
@@ -0,0 +1,51 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./generateMarkdown");
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your Project Title?',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a description?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please provide installation instructions.',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please provide usage information.',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Please provide contribution guidelines.',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'Please provide test instructions.',
+      name: 'tests',
+    },
+    {
+      type: 'list',
+      message: 'What is the License?',
+      name: 'license',
+      choices: ['MIT', 'Apache', 'Mozilla'],
+    }
+  ])
+  .then((data) => {
+    const text = generateMarkdown(data)
+
+    fs.writeFile('README.md', text, (err) =>
+      err ? console.log(err) : console.log('Success!'),
+
+    )
+  })
